323d19d82eeb79cd6ebf4d3c3445540db9cef1b9
omonmunduz <omurbekn@gmail.com>
2021-04-12 11:04:35 -0700

    next step is to add content

--- src/pages/index.js
+++ src/pages/index.js
@@ -63,6 +63,10 @@ const IndexPage = () => {
           </animated.div>
         </div>
       </div>
+      <div className="bg-indigo-300">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#140035" fill-opacity="1" d="M0,128L48,117.3C96,107,192,85,288,90.7C384,96,480,128,576,149.3C672,171,768,181,864,192C960,203,1056,213,1152,192C1248,171,1344,117,1392,90.7L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+      </div>
+      
       <About />
       <div className="bg-indigo-200 relative z-15 -mb-16">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
